d2f8d237541af95cf7ef8f736ec6b76ef7dbc7f7
Amanda Escaba <amandaescaba@gmail.com>
2021-12-14 22:20:53 -0500

    header links are now an unordered list

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import { Link } from "gatsby"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
+      // background: `rebeccapurple`,
       marginBottom: `1.45rem`,
     }}
   >
@@ -20,18 +20,25 @@ const Header = ({ siteTitle }) => (
         <Link
           to="/"
           style={{
-            color: `white`,
+            // color: `white`,
             textDecoration: `none`,
           }}
         >
           {siteTitle}
         </Link>
       </h1>
-              {/* i could make this more efficient later but im really only ever gonna have a few links its FINE!!!! */}
-              <Link to="/digital">Digital</Link>
-        <Link to="/traditional">Traditional</Link>
-        <Link to="/cards">Cards</Link>
-        <Link to="/about">About</Link>
+      {/* i could make this more efficient later but im really only ever gonna have a few links its FINE!!!! */}
+      <ul style={{
+        // marginLeft: '0',
+        // listStyleType:'none',
+        // textAlign: 'right'
+        }}
+      >
+        <li><Link to="/digital">Digital</Link></li>
+        <li><Link to="/traditional">Traditional</Link></li>
+        <li><Link to="/cards">Cards</Link></li>
+        <li><Link to="/about">About</Link></li>
+      </ul>
     </div>
   </header>
 )
