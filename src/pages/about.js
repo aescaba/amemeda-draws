1916411512911596de5c8ecc5f470d3b64de2a21
Amanda Escaba <amandaescaba@gmail.com>
2021-12-14 23:25:49 -0500

    edited about copy

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,9 +7,13 @@ import Seo from "../components/seo"
 const About = () => (
   <Layout>
     <Seo title="About amemeda" />
-    <h1>About me</h1>
-    <p>this is where my about me goes</p>
-    {/* <Link to="/">Go back to the homepage</Link> */}
+    <h1>About</h1>
+    <p>amemeda, or Amanda, is a developer and designer from the New York Metropolitan Area who likes to draw in her free time. 
+      She discovered her love of traditional art at a young age, and continued to foster her skills throughout adolescence and adulthood, 
+      most recently picking up digital art in 2020. Amanda enjoys branching out into other mediums and loves every aspect of the creative process. </p>
+
+      {/* insert contact form here*/}
+      <a href="https://www.instagram.com/amemedadraws/" target="_blank">instagram</a>
   </Layout>
 )
 
