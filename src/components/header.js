6b2c6d59a4591a9f524a2832a0dc42a133d2fc35
Amanda Escaba <amandaescaba@gmail.com>
2021-12-14 22:49:40 -0500

    navbar is now sticky and links are mapped instead of hardcoded

--- src/components/header.js
+++ src/components/header.js
@@ -2,17 +2,39 @@ import * as React from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
 
+const links = [
+  {
+    name: 'Digital',
+    url: '/digital'
+  },
+  {
+    name: 'Traditional',
+    url: '/traditional'
+  },
+  {
+    name: 'Cards',
+    url: '/cards'
+  },
+  {
+    name: 'About',
+    url: '/about'
+  },
+]
+
 const Header = ({ siteTitle }) => (
   <header
     style={{
       // background: `rebeccapurple`,
       marginBottom: `1.45rem`,
+      position: 'fixed',
+      top: '0',
+      width: '100%'
     }}
   >
     <div
       style={{
         margin: `0 auto`,
-        maxWidth: 960,
+        // maxWidth: 960,
         padding: `1.45rem 1.0875rem`,
       }}
     >
@@ -22,23 +44,34 @@ const Header = ({ siteTitle }) => (
           style={{
             // color: `white`,
             textDecoration: `none`,
+            float: 'left'
+            
           }}
         >
           {siteTitle}
         </Link>
       </h1>
-      {/* i could make this more efficient later but im really only ever gonna have a few links its FINE!!!! */}
-      <ul style={{
-        // marginLeft: '0',
-        // listStyleType:'none',
-        // textAlign: 'right'
+
+      <ul 
+        style={{
+          marginLeft: '0',
+          listStyleType:'none',
+          float: 'right'
         }}
-      >
-        <li><Link to="/digital">Digital</Link></li>
-        <li><Link to="/traditional">Traditional</Link></li>
-        <li><Link to="/cards">Cards</Link></li>
-        <li><Link to="/about">About</Link></li>
+        >
+        {links.map(link => (
+            <li
+            style={{
+              display: 'inline',
+              marginRight: '1rem',
+              
+            }}
+            >
+              <Link to={link.url}>{link.name}</Link></li>
+          )
+        )}
       </ul>
+
     </div>
   </header>
 )
